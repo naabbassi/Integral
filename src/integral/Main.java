05460b820eee5f1a8be994c761cd9f3abd62f934
Nasser <naser.abbasi447@gmail.com>
2017-07-22 09:13:11 +0200

    Sin Vector Visualization

--- src/integral/Main.java
+++ src/integral/Main.java
@@ -1,12 +1,31 @@
 package integral;
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 public class Main {
-	public static final double Increment = 1E-3;
-	
+	public static final double Increment = 1E-4;
+	static boolean drawConnectingLine = true;
 	public static void main(String[] args) {
-		System.out.println(integral(0,3, x-> {
-		 return Math.sin(x);
+		System.out.println(integral(5,2, x-> {
+		 return Math.sin(90);
 		}));
+		 SwingUtilities.invokeLater(new Runnable()
+	        {
+	            public void run()
+	            {
+	                JFrame frame = new JFrame("Test");
+	                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  
+	                Sinusoid s = new Sinusoid();
+	                s.setPreferredSize(new Dimension(600, 600));
+	                frame.getContentPane().add(s);
+	  
+	                frame.pack();
+	                frame.setVisible(true);
+	            }
+	        });
 	}
 	
 	public static double integral(double a, double b, Function function){
@@ -25,5 +44,42 @@ public class Main {
 		}
 		return((Math.round(area * 1000.0)) / 1000.0) * modifier;
 	}
+	
+	 static class Sinusoid extends JPanel
+	    {
+	        int previousY = 0;
+	 
+	        double degToRad (int deg)
+	        {
+	            return ((2*Math.PI)/360.0) * deg;
+	        }
+	 
+	        int scale (int i, int width)
+	        {
+	            return (int) ((i/(double)width)*720);
+	        }
+	 
+	        public void paintComponent (Graphics g)
+	        {
+	            super.paintComponent(g);
+	            int width = getWidth();
+	            int height = getHeight();
+	        	g.drawLine(0,height/2,width,height/2); // x-axis
+	            g.drawLine(width/2,0,width/2,height); // y-axis
+	      
+	            g.setColor(Color.red);
+	            for (int i=0; i<width; i++)
+	            {
+	                int y = (int) Math.round((-Math.sin(degToRad(scale(i,width)))+1)*height/2.0);
+	                if (drawConnectingLine && i>0)
+	                {
+	                    g.drawLine(i-1, previousY, i, y);
+	                } else {
+	                    g.drawLine(i, y, i, y);
+	                }
+	                previousY = y;
+	            }
+	        }
+	    }
 
 }
